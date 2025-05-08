3aebd4b4efbb6eba05575b2a5233b9380a9967f9
Chintan <patelchintan18018@gmail.com>
2025-05-08 19:12:51 +0530

    first commit

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  // baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://vercel.com/chintans-projects-764e7bec/to-do-raw-eng-old/api',
 });
 
 export default instance;
