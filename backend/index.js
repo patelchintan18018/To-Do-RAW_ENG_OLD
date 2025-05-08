ae1fd2ed2c7e3ac1c0390050263a5b62cf56943f
Chintan <patelchintan18018@gmail.com>
2025-05-08 18:57:30 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -25,13 +25,7 @@ app.use('/', (req,res) =>{
     })
 })
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*", (req,res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    })
-}
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
